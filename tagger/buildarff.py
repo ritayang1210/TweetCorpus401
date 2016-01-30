ed3cf1cc00567ebbf305b461ca3078393f8c15d1
yang ran <yangran1210@gmail.com>
2016-01-29 22:10:52 -0500

    init buildarff.py

---
+++ tagger/buildarff.py
@@ -0,0 +1,54 @@
+def isFrsPersonPron(token):
+    return False
+
+def isSecPersonPron(token):
+    return False
+
+def isThirdPersonPron(token):
+    return False
+
+def isCoordConj(token):
+    return False
+
+def isPastTenseVerbs(token):
+    return False
+
+def isFutureTenseVerbs(token):
+    return False
+
+def isCommas(token):
+    return False
+
+def isColonsSemi_colons(token):
+    return False
+
+def isDashes(token):
+    return False
+
+def isParentheses(token):
+    return False
+
+def isEllipses(token):
+    return False
+
+def isCommonNouns(token):
+    return False
+
+def isProperNouns(token):
+    return False
+
+def isAdverbs(token):
+    return False
+
+def iswhWords(token):
+    return False
+
+def isModernSlangAcroynms(token):
+    return False
+
+def isUpperCaseWord(token):
+    return False
+
+# def avgLenOfSentences(tweet):
+#     return
+

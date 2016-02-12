b80f4ae0bae4ea5483401743d662e8592cc5ce47
yang ran <yangran1210@gmail.com>
2016-02-11 23:29:57 -0500

    no message

--- tagger/buildarff.py
+++ tagger/buildarff.py
@@ -21,135 +21,212 @@ SLANGS = [x.lower() for x in open(PATH_WORDLISTS + '/Slang', 'r').read().splitli
 ATTR = "@attribute"
 
 def isFrsPersonPron(tokenTag):
+    '''
+    First person Pron identifier.
+    '''
     token = getToken(tokenTag)
 
     return token.lower() in FIRST_PERSON_PRON
 
 def isSecPersonPron(tokenTag):
+    '''
+    Second person Pron identifier.
+    '''
     token = getToken(tokenTag)
 
     return token.lower() in SECOND_PERSON_PRONOUNS
 
 def isThirdPersonPron(tokenTag):
+    '''
+    Third person Pron identifier.
+    '''
     token = getToken(tokenTag)
 
     return token.lower() in THIRD_PERSON_PRON
 
 def isCoordConj(tokenTag):
+    '''
+    Coordinating conjunctions identifier.
+    '''
     token = getToken(tokenTag)
 
     return token.lower() in COORD_CONJUNCTIONS or getTag(tokenTag).upper() == 'CC'
 
 def isPastTenseVerbs(tokenTag):
+    '''
+    Past-tense verbs identifier.
+    '''
     tag = getTag(tokenTag)
 
     return tag.upper() in PAST_TENSE_VERBS_TAG
 
 def isFutureTenseVerbs(tokenTag):
+    '''
+    Future-tense verbs identifier.
+    '''
     token = getToken(tokenTag)
 
     return token.lower() in FUTURE_TENSE
 
 def isCommas(tokenTag):
+    '''
+    Commas identifier.
+    '''
     token = getToken(tokenTag)
 
     return token == ","
 
 def isColonsSemiColons(tokenTag):
+    '''
+    Colons and semi-colons identifier.
+    '''
     token = getToken(tokenTag)
 
     return token in COLONS_SEMI_COLONS
 
 def isDashes(tokenTag):
+    '''
+    Dashes identifier.
+    '''
     token = getToken(tokenTag)
 
     return re.match("-+", token) != None
 
 def isParentheses(tokenTag):
+    '''
+    Parentheses identifier.
+    '''
     token = getToken(tokenTag)
 
     return token in PARENTHESES
 
 def isEllipses(tokenTag):
+    '''
+    Ellipses identifier.
+    '''
     token = getToken(tokenTag)
 
     return re.match("\.{3,}", tokenTag) != None
 
 def isCommonNouns(tokenTag):
+    '''
+    Common nouns identifier.
+    '''
     tag = getTag(tokenTag)
 
     return tag.upper() in COMMON_NOUNS
 
 def isProperNouns(tokenTag):
+    '''
+    Proper nouns identifier.
+    '''
     tag = getTag(tokenTag)
 
     return tag.upper() in PROPER_NOUNS_TAG
 
 def isAdverbs(tokenTag):
+    '''
+    Adverbs identifier.
+    '''
     tag = getTag(tokenTag)
 
     return tag.upper() in ADVERBS
 
 def isWhWords(tokenTag):
+    '''
+    wh-words identifier.
+    '''
     tag = getTag(tokenTag)
 
     return tag.upper() in WH_WORDS_TAG
 
 def isModernSlangAcroynms(tokenTag):
+    '''
+    Modern slang acroynms identifier.
+    '''
     token = getToken(tokenTag)
 
     return token.lower() in SLANGS
 
 def isUpperCaseWord(tokenTag):
+    '''
+    Words all in upper case identifier.
+    '''
     token = getToken(tokenTag)
 
     return len(token) >= 2 and token.isupper()
 
 def getToken(tokenTag):
+    '''
+    Get the token from given token/tag pair.
+    '''
     return tokenTag.split('/')[0]
 
 def getTag(tokenTag):
+    '''
+    Get the tag from given token/tag pair.
+    '''
     return tokenTag.split('/')[1]
 
 def getSentenceLength(sentence):
+    '''
+    Get length of given sentence.
+    '''
     return len(list(re.finditer("\S+/\S+\s", sentence)))
 
 def isWordToken(token):
+    '''
+    Identify if given token is a word.
+    '''
     pattern = re.compile("^\w+$")
 
     return pattern.match(token) != None
 
 def gatherFeatureInfo(fileName, maxNumOfTweetsPerClass):
+    '''
+    Gather the feature information from a given file. Each class will not contain more feature lines than the maxNumOfTweetsPerClass provided.
+    '''
     twtFile = open(fileName, 'r')
     numPerClass = {}
     for classToCompute in CLASSES_TO_COMPUTE:
+        # Init the counter for each class
         numPerClass[classToCompute] = 0
     taggedTweets = twtFile.read()
+    # regex used to fetch the next tweet
     tweets = re.finditer("<A=(\d)>\n((?:\S+/\S+\s+)+)", taggedTweets)
     res = []
+    # Init the list of identifiers
     tokenIdentifiers = [isFrsPersonPron, isSecPersonPron, isThirdPersonPron, isCoordConj, isPastTenseVerbs, isFutureTenseVerbs, isCommas, isColonsSemiColons, isDashes, isParentheses, isEllipses, isCommonNouns, isProperNouns, isAdverbs, isWhWords, isModernSlangAcroynms, isUpperCaseWord]
     for tweet in tweets:
+        # Process each tweet
         twtInfo = [0] * 21
         tweetClass = int(tweet.group(1))
         twtInfo[-1] = tweetClass
         if not tweetClass in numPerClass or numPerClass[tweetClass] >= maxNumOfTweetsPerClass:
+            # Skip if already exceed the maxNumOfTweetsPerClass for this class
             continue
         numPerClass[tweetClass] = numPerClass[tweetClass] + 1
+
+        # Split tweet into sentences
         sentences = filter(None, tweet.group(2).split('\n'))
         numOfSentence = len(sentences)
         twtInfo[-2] = numOfSentence
         numOfWordTokens = 0
         for sentence in sentences:
+            # Calculate sentence length
             twtInfo[-4] = twtInfo[-4] + getSentenceLength(sentence)
             tokenTags = re.finditer("(\S+/\S+)", sentence)
             for tokenTag in tokenTags:
                 tokenTagText = tokenTag.group()
                 token = getToken(tokenTagText)
                 if (isWordToken(token)):
+                    # Count length of each word token
                     numOfWordTokens += 1
                     twtInfo[-3] = twtInfo[-3] + len(token)
                 for i in range(len(tokenIdentifiers)):
+                    # Run each identifier for each token
                     twtInfo[i] = twtInfo[i] + tokenIdentifiers[i](tokenTagText)
+        # Calculate averages
         twtInfo[-4] = float(twtInfo[-4]) / numOfSentence
         twtInfo[-3] = float(twtInfo[-3]) / numOfWordTokens
         res.append(twtInfo)
@@ -159,6 +236,8 @@ if __name__ == "__main__":
     inputFile = ''
     outputFile = ''
     maxNumOfTweetsPerClass = sys.maxint
+
+    # Read command line inputs first
     if len(sys.argv) == 3:
         inputFile = sys.argv[1]
         outputFile = sys.argv[2]
@@ -170,6 +249,7 @@ if __name__ == "__main__":
         print 'usage: buildarff.py <inputFile> <outputFile> [<maxNumOfTweetsPerClass>]'
         sys.exit()
 
+    # Build the .arff file
     esFile = open(outputFile, "w")
     esFile.write("@relation TweetsFeatureInformation\n\n")
     esFile.write(ATTR + " FirstPersonPronouns numeric\n")
--- tagger/twtt.py
+++ tagger/twtt.py
@@ -13,37 +13,58 @@ ABBREV_FILES = ['abbrev.english', 'pn_abbrev.english', 'pn_abbrev.english2']
 SUFFIX_ABBREV = '_DOT'
 
 def read_file(file_name):
-    """(str of file name) ->Table
-    Input a string which is the file name of a table
-    """
+    '''
+    Read a file with given file name and return all lines in that file.
+    '''
     file = open(file_name, 'r')
-    # read table to get all of the content of the table
     lines = file.readlines()
     file.close()
     return lines
 
 def extractTweetText(line):
+    '''
+    Extract the tweet text from a given line.
+    '''
     line_list = line.split(',"')
     return line_list[INDEX_TWEET_TEXT].strip()[:-1].strip()
 
 def removeHTMLTagAttr(sentence):
+    '''
+    Remove all HTML tags and attributes in the given sentence.
+    '''
     return re.sub('<[^<]+?>', '', sentence)
 
 def replaceHTMLChars(sentence):
+    '''
+    Replace all HTML characters in the given sentence.
+    '''
     parser = HTMLParser()
     return parser.unescape(sentence)
 
 def removeURL(sentence):
+    '''
+    Remove URL's in the given sentence.
+    '''
     return re.sub(r"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)", "", sentence)
 
 def removeFirstCharOfUserNameHashTag(sentence):
+    '''
+    Remove first character of user name and hash tag.
+    '''
     return re.sub(r"[@|#](\w+)", r"\1", sentence)
 
 def getSentences(tweetText):
+    '''
+    Return a list of sentences given the tweet text.
+    '''
     return re.split(r'(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?|\!)\s?', tweetText)
 
 def decorateAbbrev(tweetText):
+    '''
+    Replace '.' in abbreviations with '_DOT'. 
+    '''
     for abbrevFilePath in ABBREV_FILES:
+        # Read the abbreviation file
         abbrevFile = open(ABBREV_FILES_PATH + abbrevFilePath, 'r')
         for line in abbrevFile.readlines():
             line = line.strip()
@@ -52,19 +73,27 @@ def decorateAbbrev(tweetText):
     return tweetText
 
 def getTokenTagList(nlp, sentence):
+    '''
+    Return the token/tag list from a given sentence.
+    '''
     it = re.finditer(r"\w+|'\w+|[^\w\s]+", sentence)
     tokens = []
     for match in it:
+        # Put the '.' back for each occurence of '_DOT'
         tokens.append(match.group().replace(SUFFIX_ABBREV, '.'))
 
     nlpRes = nlp.tag(tokens)
     res = []
     for i in range(len(tokens)):
+        # Build the token/tag pair
         comb = tokens[i] + '/' + nlpRes[i]
         res.append(comb)
     return res
 
 def getDemarcation(line):
+    '''
+    Get the demarcation from a given line.
+    '''
     line_list = line.split(',"')
     return "<A=" + line_list[INDEX_DEMARCATION][1] +">"
 
@@ -72,6 +101,8 @@ if __name__ == "__main__":
     inputFile = ''
     outputFile = ''
     groupNumber = None
+
+    # Read command line inputs first
     if len(sys.argv) == 3:
         inputFile = sys.argv[1]
         outputFile = sys.argv[2]
@@ -85,6 +116,7 @@ if __name__ == "__main__":
 
     lines = read_file(inputFile)
 
+    # Extract the training data using given group number
     trainingData = []
     if groupNumber:
         trainingData = lines[groupNumber * 5500: (groupNumber + 1) * 5500] + lines[800000 + groupNumber * 5500: 800000 + (groupNumber + 1) * 5500]
@@ -94,11 +126,13 @@ if __name__ == "__main__":
     resFile = open(outputFile, "w")
     nlp = NLPlib.NLPlib()
     for line in trainingData:
+        # Parse each line in the training data and build the token/tag pairs
         resFile.write(getDemarcation(line))
         resFile.write("\n")
         tweetText = extractTweetText(line)
         tweetText = decorateAbbrev(tweetText)
         for sentence in getSentences(tweetText):
+            # Process each sentence from each line
             sentence = removeURL(sentence)
             sentence = replaceHTMLChars(sentence)
             sentence = removeFirstCharOfUserNameHashTag(sentence)
@@ -106,6 +140,7 @@ if __name__ == "__main__":
             sentence = sentence.strip()
 
             tokenTagList = getTokenTagList(nlp, sentence)
+            # Write the result into the output file
             for tokenTag in tokenTagList:
                 resFile.write(tokenTag + " ")
             resFile.write("\n")
